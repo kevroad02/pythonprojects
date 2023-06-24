e7f3d4b7f9f4654f1e977065bb95a482e288ef4e
Kevin Roadarmel <goldenrobloxian@gmail.com>
2023-06-24 13:48:06 -0400

    Fart

--- gitHaven.sh
+++ gitHaven.sh
@@ -3,7 +3,10 @@ cd /storage/emulated/0/Documents/Termux/
 git add .
 git config --global user.name "Kevin Roadarmel" 
 git config --global user.email "goldenrobloxian@gmail.com"
-git commit -m "Update"
+echo Enter a concise summary of what you edited
+read varname
+git commit -m $varname
+echo Pushed to GitHub with update name $varname
 git remote add origin https://github.com/kevroad02/pythonprojects.git
 git push -u origin master
 
