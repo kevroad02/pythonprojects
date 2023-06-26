a3e5251c15888b23ccac967f3dc1fc50b2c34032
Kevin Roadarmel <goldenrobloxian@gmail.com>
2023-06-26 17:10:36 -0400

    test

--- tax.py
+++ tax.py
@@ -1,77 +1,90 @@
-wkOrBi = ""
-stage = 0
-intNumeric = True
+# Global variable for how many hours have been worked
+dwHours = 0
+
+# Global num used for intPut function results
+num = 0
+
+# Here's the global tax number if you want to change that
+tax = 0.13
 
 
-def checkInt(number):
-    if number.isnumeric():
-        intNumeric = True
+
+def intPut(question):
+    global num
+    # Gets string intPut to ask the questions 
+    theInput = input(question)
+    # If input is an integer
+    if theInput.isnumeric():
+        num = int(theInput)
     else:
-        intNumeric = False
+        print("\nPlease enter a number without decimals.")
+        # Re-asks question if false
+        intPut(question)
     
+
+
 
 def daysWorked():
-    global stage
-    run = True
-    while run:
-        if stage == 0:
-            day = "Sunday"
-            dayWorked = input("How many hours did you work on " + day)
-            checkInt(dayWorked)
-            stage = 1
+    global dwHours
+    global num
+    days = ["Sunday: ", "Monday: ","Tuesday: ", "Wednesday: ","Thursday: ","Friday: ","Saturday: "]
+    howMany = "\nHow many hours did you work on "
+    # Runs through days of the week list and adds up each number responses
+    for i in days:
+        dayWorked = intPut(howMany + i)
+        dwHours = dwHours + num
+    
+    print("\nYou have entered in working " + str(dwHours) + " hours.")
         
-        elif stage == 1:
-            day = "Monday"
-            dayWorked = input("How many hours did you work on " + day)
-            checkInt(dayWorked)
-            stage = 2
-        
-        elif stage == 2:
-            day = "Tuesday"
-            dayWorked = input("How many hours did you work on " + day)
-            checkInt(dayWorked)
-            stage = 3
-        elif stage == 3:
-            day = "Wednesdau"
-            dayWorked = input("How many hours did you work on " + day)
-            checkInt(dayWorked)
-            stage = 4
-        elif stage == 4:
-            day = "Thursday"
-            dayWorked = input("How many hours did you work on " + day)
-            checkInt(dayWorked)
-            stage = 5
-        elif stage == 5:
-            day = "Friday"
-            dayWorked = input("How many hours did you work on " + day)
-            checkInt(dayWorked)
-            stage = 6
-        elif stage == 6:
-            day = "Saturday"
-            dayWorked = input("How many hours did you work on " + day)
-            checkInt(dayWorked)
-            run = False
+    
+    
+    
+    
     
 def approximate(wk):
-    global wkOrBi
-    if wkOrBi == "W":
-        hoursWorked = input("\nHow many hours did you work this week? Input IDK for \"I don't know\": ")
-        if hoursWorked.upper() == "IDK":
+    global dwHours
+    global tax
+    # If weekly
+    if wk == "W":
+        hoursWorked = input("\nDo you know how many hours you've worked this week? Y or N: ")
+        if hoursWorked.upper() == "Y":
+            intPut("\nEnter how many hours you've worked: ")
+            # intPut responses are always converted to nums so dwHours now equals how many hours user has worked
+            dwHours = num
+        else:
+            # daysWorked function goes through each day and asked them how mant hours they've worked on that dau
             daysWorked()
-            
+        perHourQ = intPut("\nWhat is your salary per hour?: ")
+        perHour = num
+        # Hourly salary times hours worked minus taxes
+        print("\nYou will be paid $" + str(((perHour*dwHours) - (num*tax))))
+    elif wk == "BW":
+        hoursWorked = input("\nDo you know how many hours you've worked over these two week? Y or N: ")
+        if hoursWorked.upper() == "Y":
+            intPut("\nEnter how many hours you've worked: ")
+            dwHours = num
+        else:
+            # Asks to enter each week of work, data combines automatically since it just keeps adding together in the dwHours variable 
+            print("\nEnter your first week of work")
+            daysWorked()
+            print("\nEnter your second week of work")
+            daysWorked()
+        perHourQ = intPut("\nWhat is your salary per hour?: ")
+        perHour = num
+        print("\nYou will be paid $" + str(((perHour*dwHours) - (num*tax))))
     
 
 
 
-
-weekAmount = input("Pennsylvania Paycheck Approximator\nKevin Roadarmel\n\nEnter W if you get paid weekly, or BW for biweekly: ")
+# Beginning of program, asks if looking for weekly or biweekly results
+weekAmount = input("Pennsylvania Paycheck Approximator (13% Tax)\nKevin Roadarmel\n\nEnter W if you get paid weekly, or BW for biweekly: ")
 if weekAmount.upper() == "BW":
-    wkOrBi
     print("\nBiweekly has been selected")
     wkOrBi = "BW"
+    # Sends biweekly to function 
     approximate(wkOrBi)
 if weekAmount.upper() == "W":
-    wkOrBi
     print("\nWeekly has been selected")
+    # Sends weekly to function
     wkOrBi = "W"
     approximate(wkOrBi)
