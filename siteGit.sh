bf0bed9e21040540876598eb1a340461fc6318d5
Kevin Roadarmel <goldenrobloxian@gmail.com>
2023-06-27 15:19:27 -0400

    Update

---
+++ siteGit.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+cd /storage/emulated/0/Documents/rogbone.com/
+git init
+git add .
+git config --global user.name "Kevin Roadarmel" 
+git config --global user.email "goldenrobloxian@gmail.com"
+echo Enter a concise summary of what you edited with underscores as spaces
+read varname
+git commit -m $varname
+echo Pushed to GitHub with update name $varname
+git remote add origin https://github.com/kevroad02/rogbone.com.git
+git push -u origin master
+
